ab2d8e7e529ec121a0d468b3ea7583da0d402e94
hyein0112 <bhi12134@gmail.com>
2022-03-07 20:57:48 +0900

    add MyVStackView

--- SwiftUI_tutorial_#1/SwiftUI_tutorial_#1/ContentView.swift
+++ SwiftUI_tutorial_#1/SwiftUI_tutorial_#1/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack{
+            MyVStackView()
+            MyVStackView()
+            MyVStackView()
+        }
+        .padding(10)
+        // default: .all
+        .background(Color.yellow)
     }
 }
 
---
+++ SwiftUI_tutorial_#1/SwiftUI_tutorial_#1/MyVStackView.swift
@@ -0,0 +1,28 @@
+//
+//  MyVStackView.swift
+//  SwiftUI_tutorial_#1
+//
+//  Created by 혜인 on 2022/03/07.
+//
+
+import SwiftUI
+
+
+struct MyVStackView: View{
+    var body: some View{
+        
+        VStack{
+            Text("1!")
+                .fontWeight(.bold)
+                .font(.system(size: 40))
+            Text("2!")
+                .fontWeight(.bold)
+                .font(.system(size: 40))
+            Text("3!")
+                .fontWeight(.bold)
+                .font(.system(size: 40))
+        }
+        .background(Color.red)
+        
+    }
+}
