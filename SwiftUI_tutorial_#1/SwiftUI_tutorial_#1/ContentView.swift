bbc60a90e58f95bc397d086abebbacc94b27aa73
hyein0112 <bhi12134@gmail.com>
2022-03-07 20:13:21 +0900

    [CREATE] Tutorial_#1

---
+++ SwiftUI_tutorial_#1/SwiftUI_tutorial_#1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_tutorial_#1
+//
+//  Created by 혜인 on 2022/03/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
