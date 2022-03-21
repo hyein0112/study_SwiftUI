5c2e6c5097118f6c184ea2f61366463fa6f683fd
hyein0112 <bhi12134@gmail.com>
2022-03-21 14:58:10 +0900

    RoundedRectangle, Circle 추가

--- SwiftUI_WebView_Tutorial/SwiftUI_WebView_Tutorial/ContentView.swift
+++ SwiftUI_WebView_Tutorial/SwiftUI_WebView_Tutorial/ContentView.swift
@@ -9,33 +9,62 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        
         NavigationView{
-            HStack{
-                NavigationLink(destination:  MyWebView(urlToLoad: "http://www.daum.net")){
-                    Image("daum")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(height: 40)
-                }
-                .padding()
+            VStack{
+                VStack{
                     
-                NavigationLink(destination: MyWebView(urlToLoad: "http://www.naver.com")){
-                    Image("naver")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(height: 25)
                 }
-                .padding()
+                RoundedRectangle(cornerSize: CGSize(width: 20, height: 20))
+                    .offset(y: -150)
+                    .foregroundColor(Color.yellow)
                 
-                NavigationLink(destination: MyWebView(urlToLoad: "http://www.google.com")){
-                    Image("google")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(height: 40)
-                }
-                .padding()
+                Circle()
+                    .frame(width: 200)
+                    .foregroundColor(.yellow)
+                    .shadow(color: .gray, radius: 2, x: 5, y: 5)
+                    .overlay(
+                        Circle()
+                            .opacity(0.1)
+                    )
+                    .overlay(
+                        Circle().stroke(Color.green, lineWidth: 5)
+                    )
+                    .overlay(
+                        Text("이미지를 클릭하여 웹사이트로 이동")
+                            .font(.system(size: 20))
+                            .fontWeight(.bold)
+                            .multilineTextAlignment(.center)
+                    )
+                    .offset(y: -300)
+                
+                HStack{
+                    NavigationLink(destination:  MyWebView(urlToLoad: "http://www.daum.net").edgesIgnoringSafeArea(.all)){
+                        Image("daum")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(height: 36)
+                    }
+                    .padding()
+                        
+                    NavigationLink(destination: MyWebView(urlToLoad: "http://www.naver.com").edgesIgnoringSafeArea(.all)){
+                        Image("naver")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(height: 25)
+                    }
+                    .padding()
+                    
+                    NavigationLink(destination: MyWebView(urlToLoad: "http://www.google.com").edgesIgnoringSafeArea(.all)){
+                        Image("google")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(height: 40)
+                    }
+                    .padding()
+                }.offset(y: -250)
+                    .shadow(color: .gray, radius: 1.5, x: 2, y: 4)
             }
+            
         }
 
     }
