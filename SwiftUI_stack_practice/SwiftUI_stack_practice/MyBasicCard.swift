c910943fb2ba7d8a597bd2b2f5869c9eeb269089
hyein0112 <bhi12134@gmail.com>
2022-04-09 21:39:46 +0900

    create MyBasicCard

---
+++ SwiftUI_stack_practice/SwiftUI_stack_practice/MyBasicCard.swift
@@ -0,0 +1,20 @@
+//
+//  MyBasicCard.swift
+//  SwiftUI_stack_practice
+//
+//  Created by 혜인 on 2022/04/09.
+//
+
+import SwiftUI
+
+struct MyBasicCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MyBasicCard_Previews: PreviewProvider {
+    static var previews: some View {
+        MyBasicCard()
+    }
+}
