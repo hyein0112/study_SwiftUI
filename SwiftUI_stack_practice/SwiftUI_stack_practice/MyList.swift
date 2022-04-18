69c54ba8888c41528bc63db0efb1819f2919288c
hyein0112 <bhi12134@gmail.com>
2022-04-18 14:33:02 +0900

    create MyList

---
+++ SwiftUI_stack_practice/SwiftUI_stack_practice/MyList.swift
@@ -0,0 +1,20 @@
+//
+//  MyList.swift
+//  SwiftUI_stack_practice
+//
+//  Created by 혜인 on 2022/04/18.
+//
+
+import SwiftUI
+
+struct MyList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MyList_Previews: PreviewProvider {
+    static var previews: some View {
+        MyList()
+    }
+}
